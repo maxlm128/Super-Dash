a614e940ec88dc99ffc63a1f643e5ae866a8c1cf
maxlm128 <maxim.derksen@web.de>
2020-12-08 15:25:29 +0100

    Added class menu (btw first commit)

---
+++ Game/src/main/Map.java
@@ -0,0 +1,30 @@
+package main;
+
+import com.asecave.Console;
+
+class Map {
+	int width;
+	int height;
+	static char[][] map;
+	static int[][] trailMap;
+	Console console;
+	Screen screen;
+	Player player;
+
+	public Map() {
+	}
+
+// creates the new array and saves the borders of the map into the new array
+	public void newMapArray() {
+		map = MapLoader.charsgetMap();
+		width = map.length;
+		height = map[0].length;
+		trailMap = new int[width][height];
+		map[player.coordsXPlayer][player.coordsYPlayer] = '<';
+	}
+
+//new reference to the class Player
+	public void newPlayerReference(Player pPlayer) {
+		player = pPlayer;
+	}
+}
